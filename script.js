81a195b2513f3b1c17ca6cabd53cdfcf9c5f6342
Freja Fischer <frej657e@stud.kea.dk>
2023-09-14 13:28:13 +0200

    deleted an console.log

--- script.js
+++ script.js
@@ -246,7 +246,7 @@ function filtering(evt) {
   document.querySelector(".filter_cat .work").classList.remove("work_active");
   document.querySelector(".filter_cat .study").classList.remove("study_active");
   document.querySelector(".filter_cat .home").classList.remove("home_active");
-  console.log(evt.target);
+
   const clicked = evt.target.dataset.filter;
   document.querySelector("#list tbody").innerHTML = "";
   document.querySelector("#done tbody").innerHTML = "";
